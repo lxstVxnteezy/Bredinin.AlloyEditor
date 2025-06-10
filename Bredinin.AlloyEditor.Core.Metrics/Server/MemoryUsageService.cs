693a3fd73fd2665038cb9a31267b3a98829a877a
Vanteezy <Vanteezy@MONSTOR>
2025-06-10 21:23:39 +0300

    update

--- Bredinin.AlloyEditor.Core.Metrics/Server/MemoryUsageService.cs
+++ Bredinin.AlloyEditor.Core.Metrics/Server/MemoryUsageService.cs
@@ -7,8 +7,8 @@ namespace Bredinin.AlloyEditor.Core.Metrics.Server
     public class MemoryUsageService : BackgroundService
     {
         private readonly Gauge _memoryGauge = Prometheus.Metrics.CreateGauge(
-            "process_memory_usage_megabytes",  // Изменили название метрики
-            "Current memory usage in megabytes (MB)");  // Обновили описание
+            "process_memory_usage_megabytes",
+            "Current memory usage in megabytes (MB)");  
 
         private readonly TimeSpan _delay = TimeSpan.FromSeconds(15);
 
@@ -18,10 +18,9 @@ namespace Bredinin.AlloyEditor.Core.Metrics.Server
             {
                 var process = Process.GetCurrentProcess();
 
-                // Конвертируем байты в мегабайты (1 МБ = 1024 * 1024 байт)
                 double memoryUsageMb = process.WorkingSet64 / (1024.0 * 1024.0);
 
-                _memoryGauge.Set(memoryUsageMb);  // Записываем значение в МБ
+                _memoryGauge.Set(memoryUsageMb); 
 
                 await Task.Delay(_delay, stoppingToken);
             }
