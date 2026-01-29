ac5a9102b16e21cf80038df2e2ae70f5ca9a73f2
Vntz <bredinin1@gmail.com>
2026-01-29 23:28:31 +0300

    1

---
+++ Bredinin.AlloyEditor.Identity.Service.Handlers/Admin/UserSearchDto.cs
@@ -0,0 +1,28 @@
+﻿using Bredinin.AlloyEditor.Identity.Service.Contracts.Queries.Admin;
+using Bredinin.AlloyEditor.Identity.Service.DAL.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bredinin.AlloyEditor.Identity.Service.Handler.Admin
+{
+    internal class UserSearchDto(IdentityDbContext context) : IRequestHandler<GetAllSearchUserQueries, SearchUserQuery[]>
+    {
+        public async Task<SearchUserQuery[]> Handle(GetAllSearchUserQueries request, CancellationToken cancellationToken)
+        {
+            var users = await context.Users
+                .Include(x => x.UserRoles)
+                .Select(x => new SearchUserQuery(
+                    x.Id,
+                    x.Login,
+                    x.FirstName,
+                    x.LastName,
+                    x.SecondName,
+                    x.Age,
+                    x.UserRoles.Select(x=>x.RoleId).ToArray()
+                    ))
+                .ToArrayAsync(cancellationToken);
+
+            return users;
+        }
+    }
+}
